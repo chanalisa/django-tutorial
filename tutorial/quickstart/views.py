47ccff371265c780bab56b2edc70787e49098b1d
Alisa Chan <alisachan.ny@gmail.com>
2020-10-06 09:32:49 -0400

    wired up the API URLs

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from django-tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
